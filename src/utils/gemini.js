50edde09bb6e6a6f9a582d913cd2c83e7257b038
Isaac Tiza <i2493053@gmail.com>
2026-05-17 15:14:49 +0100

    Fix: Removed useless comment

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-// const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 const toneInstructions = {
   simple:
